b1e5920cb82435eb97c175cc661ef2d6bd98863d
Samuel de Lorenzi Ribeiro <411845@unoesc.lan>
2023-08-30 20:30:31 -0300

    Primeiro Commit

---
+++ Atividades/OrdenacaoArray/Program.cs
@@ -0,0 +1,9 @@
+﻿int[] vetor = new int[10];
+for (int i = 0; i < vetor.Length; i++)
+{
+int b = i+1;
+Console.WriteLine("Insira o "+ b + "° dígito do vetor:");
+vetor[i] = Convert.ToInt32(Console.ReadLine());
+}
+Array.Sort(vetor);
+Console.WriteLine(string.Join(",", vetor));
